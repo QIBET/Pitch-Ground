45bf8d759d9471f54fba8cbff8c93e673b617f29
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-13 14:03:35 +0300

    Creating raltionship between models

--- app/models.py
+++ app/models.py
@@ -1,76 +1,99 @@
-from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
 from flask_login import UserMixin
+from . import login_manager 
+from datetime import datetime 
 
 
-class User(UserMixin,db.Model):
-    '''
-    This class will contain database schema for users
-    '''
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    pitches = db.relationship('Pitch',backref='user',lazy = 'dynamic')
-    comments = db.relationship('Comment',backref='user',lazy='dynamic')
-    password_hash = db.Column(db.String)
-    @property
-    def password(self):
-        '''
-        Raises error when someone trys to read the password
-        '''
-        raise AttributeError('You cannot read the password attribute')
-
-    @password.setter
-    def password(self,password):
-        '''
-        Generates password hash
-        '''
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self,password):
-        '''
-        confirms password equal to the password hash during login
-        '''
-        check_password_hash(self.password_hash,password)
 
 class Pitch(db.Model):
     '''
-    This class will contain the database schema for picthes table
+    properties of pitch class
     '''
-    __tablename__ = 'pitches'
+    __tablename__='pitches' 
 
-    id = db.Column(db.Integer,primary_key = True)
-    pitch = db.Column(db.String)
-    category = db.Column(db.String)
-    users_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(255))
+    category=db.Column(db.String(255))
+    content=db.Column(db.String(255))
+    date_posted=db.Column(db.DateTime,default=datetime.utcnow)
+    votes_id=db.Column(db.Integer,db.ForeignKey("votes.id"))
+    posted_by=db.Column(db.Integer,db.ForeignKey("users.id"))
+    users=db.relationship('User',backref="user",lazy = "dynamic")
+    pitches = db.relationship('PitchComments',lazy = "dynamic")
+
+
+
+
+class PitchComments(db.Model):
+    '''
+    model that defines the properties of comments
+    '''
+    __tablename__='comments'
+
+    id=db.Column(db.Integer,primary_key=True)
+    pitch_id=db.Column(db.Integer,db.ForeignKey("pitches.id"))
+    title=db.Column(db.String(255))
+    content=db.Column(db.String(255))
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+    date_posted=db.Column(db.DateTime,dafault=datetime.utcnow)
 
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_pitch(cls,category):
-        pitches = Pitch.query.filter_by(category=category).all()
+    def get_pitches(cls,id):
+        pitches = PitchComments.query.filter_by(pitch_id = id).all()
         return pitches
-class Comment(db.Model):
-    '''
-    This class will contain the schema for comments
-    '''
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer,primary_key = True)
-    comment = db.Column(db.String(255))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-    @classmethod
-    def get_comments(cls,pitch_id):
-        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
-        return comments
+class Votes(db.Model):
+    '''
+    model that defines properties of votes
+    '''
+    __tablename__="votes"
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    id=db.Column(db.Integer,primary_key=True)
+    vote_count=db.Column(db.Integer)
+    user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitches=db.relationship('Pitch',backref='pitches',lazy="dynamic")  
+
+
+
+class User(db.Model):
+    '''
+    models that dfeines properties of user class
+    '''
+    __tablename__="users"
+
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(255))
+    user_bio=db.Column(db.String(255))
+    photos=db.relationship('PhotoProfile',backref ='user',lazy= "dynamic")
+    pitches=db.relationship('Pitch', backref ='pitches',lazy= "dynamic")
+    pitchcomments=db.relationship('PitchComments',backref ='pitchcomments',lazy= "dynamic")
+    pitch_id=db.Column(db.Integer,db.ForeignKey("pitches.id"))
+    votes=db.relationship('Votes',backref ='votes',lazy= "dynamic")
+    email=db.Column(db.String(),unique = True,index = True)
+    role_id=db.Column(db.Integer,db.ForeignKey("roles.id"))
+    password_hash=db.Column(db.String(255)) 
+    
+
+
+class Roles(db.Model):
+    '''
+    defines the role of each user in the user model 
+    '''
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(30))
+    users = db.relationship('Users',backref='users',lazy= "dynamic")
+
+class PhotoProfile(db.Model):
+    '''
+    model that defines profile photos for user account
+    '''
+    __tablename__ ="photos"
+
+    id=db.Column(db.Integer,primary_key=True)
+    pic_path=db.Column(db.String())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
