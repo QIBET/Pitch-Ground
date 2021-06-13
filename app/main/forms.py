dc5015e152ff4737cdbd04d0f91238a27233e0e1
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-13 22:05:22 +0300

    Creates forms file for pitch input

---
+++ app/main/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms.validators import Required,Email
+from ..models import User
+
+
