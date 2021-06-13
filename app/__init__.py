129bb036026fcdd399b0ca85ec97f042aaf5c1f8
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-13 16:34:38 +0300

    Installing of psycopg2 and creating database link

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app(config_name):
 
     #initializing the flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
 
     #registering the blue print
