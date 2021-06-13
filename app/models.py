83242654acf75e29df83d70bc70143a8c7d367a9
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-13 16:10:35 +0300

    creating manager shell

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from flask_login import UserMixin
 from . import login_manager 
 from datetime import datetime 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 
 class Pitch(db.Model):
@@ -58,9 +62,11 @@ class Votes(db.Model):
     user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
     pitches=db.relationship('Pitch',backref='pitches',lazy="dynamic")  
 
+    
 
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     '''
     models that defines properties of user class
     '''
