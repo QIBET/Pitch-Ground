2223b744e1b54361bdc9fc641a431a4a6f2aa90e
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-13 22:11:50 +0300

    Creating the pitch comment form section

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
 from wtforms import StringField,TextAreaField,SubmitField,ValidationError
 from wtforms.validators import Required,Email
 from ..models import User
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators= [Required()])
+    comment = TextAreaField('Pitch Comment', validators = [Required()])
+    submit = SubmitField('Post')
+
 
 
