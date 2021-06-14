d616cfa985820bba3e41e2f702f628605a445141
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-14 19:06:22 +0300

    Creating forms to handle, upvote,downvote, commment and pitchform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,22 @@
+
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha import validators
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
-from wtforms.validators import Required,Email
-from ..models import User
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
 
 class PitchForm(FlaskForm):
     title = StringField('Pitch Title', validators= [Required()])
-    comment = TextAreaField('Pitch Comment', validators = [Required()])
+    description = TextAreaField('Pitch Comment', validators = [Required()])
+    category = RadioField('Label', choices = [('Technology','Technology'), ('FashionPitch','FashionPitch'),('MotivationalPitch','Motivational'), ('Enterpreneuralpitch','Enterpreneuralpitch')],validators=[Required()])
     submit = SubmitField('Post')
 
 
+class CommentForm(FlaskForm):
+    description = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField()
 
+class UpvoteForm(FlaskForm):
+    submit = SubmitField()
+
+class DownvoteForm(FlaskForm):
+    submit = SubmitField()
