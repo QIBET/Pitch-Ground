ede74c26ed6f36ae3ff6af8e392d98a6bf0eec96
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-14 15:02:20 +0300

    creating the config file

--- config.py
+++ config.py
@@ -7,13 +7,27 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Sotik2020*@localhost/pitchground'
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+   
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 class ProdConfig(Config):
     '''
     Production configuration child class
 
     Args:
         Config: General parent class for configuration
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
     '''
     pass
 
@@ -23,9 +37,10 @@ class DevConfig(Config):
     Args:
         Config: General parent class for configuration
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Sotik2020*@localhost/pitchground'
     DEBUG = True
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY="Benny"
+export MAIL_USERNAME="bernard.kibet@student.moringaschool.com"
+export MAIL_PASSWORD="Sotik2020*"
 
 python3.8 manage.py server
