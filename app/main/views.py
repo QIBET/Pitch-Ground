97908c1e0790df708cf5c64dc198fcf1d23d710b
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-14 19:32:33 +0300

    Creating imports on views

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for,abort,flash
+from . import main
+from flask_login import login_required, current_user
+from ..models import Pitch, User, PitchComments, Upvote, Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm
+from flask.views import View, MethodView
+from .. import db
+  
+
