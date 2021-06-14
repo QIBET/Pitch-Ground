22e88970afb475674e3de5753e58b862dd7921f1
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-14 23:59:53 +0300

    creating the link to email

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 from flask_login import UserMixin
-""" from . import login_manager  """
+from . import login_manager 
 from datetime import datetime 
 
-""" @login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
- """
+
 
 
 class Pitch(db.Model):
--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
    
+   #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
