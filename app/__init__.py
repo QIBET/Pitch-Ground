121f01b8f5beea1e4299fdc7a665965b6024b3dc
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-14 15:41:33 +0300

    Creating the app configuration and initializing flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,22 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+simple = SimpleMDE()
+
 def create_app(config_name):
 
     app = Flask(__name__)
-  
+
+    simple.init_app(app)
+ 
     
     
     #creating app configurations
@@ -19,11 +28,16 @@ def create_app(config_name):
     #initializing the flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
 
     #registering the blue print
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Regestering the auth bluprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
    
     return app
