9ed54d1e78a29e40e292ee419f410c5965054b27
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-15 08:05:14 +0300

    Creating the welcome message on email

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_simplemde import SimpleMDE
+from flask_mail import Mail
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -11,6 +13,8 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
+mail = Mail()
+
 
 def create_app(config_name):
 
@@ -29,6 +33,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
 
     #registering the blue print
--- config.py
+++ config.py
@@ -8,15 +8,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Sotik2020*@localhost/pitchground'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-   
+  
    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    
+
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
