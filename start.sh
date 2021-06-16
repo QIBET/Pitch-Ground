bde48aa8a9bac389764e799accf7a573617ee89d
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-16 13:02:10 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY="Benny"
 export MAIL_USERNAME="bernard.kibet@student.moringaschool.com"
 export MAIL_PASSWORD="Sotik2020*"
-export DATABASE_URL="postgresql+psycopg2://moringa:Sotik2020*@localhost/pitchground"
+export DATABASE_URL="postgres://ejujesuwlvuust:8648374b2b15bb86d2e77ecb60d73960f3f134407f43779957695b5bda14ec67@ec2-54-152-185-191.compute-1.amazonaws.com:5432/d3djammh9fvo2v"
 
 python3.8 manage.py server
